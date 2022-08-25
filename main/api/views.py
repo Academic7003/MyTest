3986561c1a37707df09f4d010dfb8701b1db1328
academic7003 <nazarmamatovnurmuhammad7@gmail.com>
2022-08-25 09:01:44 +0500

    id bug

--- main/api/views.py
+++ main/api/views.py
@@ -1,35 +1,31 @@
-from math import remainder
-from unicodedata import name
+from math import ceil
 from main.models import *
 from rest_framework.response import Response
 from main.api.serilaizers import *
 from rest_framework import status
 from rest_framework.decorators import api_view
-
+not_free = []
 serial_list = []
 serial_list2 = []
 
 def create_warehouse_serializer(list, obj):
-      list.append(obj)
+    
+    list.append(obj)
 
 @api_view(['POST', 'GET'])
 def create_product(request):
     context = {}
-    global z
-    global zz
 
-    
     if request.method == "GET":
-        
-        zz = WareHouseModel(material_id=1)
-        z1 = WareHouseModel(material_id=1)
-        z = WarehouseSerializer([zz,z1], many=True)
+        prod = ProductModel.objects.all()
+        serializer = ProductSerilizer(prod, many=True)
 
-        return Response(z.data)
+        return Response(serializer.data)
     else:
         serializer = ResponseMaterialSerializer(data=request.data)
         
         if serializer.is_valid():
+
             pr_id = serializer.data['product_id']
             count = serializer.data['product_qty']
             pr_id2 = serializer.data['product_id2']
@@ -37,23 +33,26 @@ def create_product(request):
 
 
             # berilgan mahsulotni bittasi u.n kerak hom ashyoni chiqarib. bizga k.k miqdorga kopaytiryapman
-            prod = ProductMaterialsModel.objects.filter(name_id=pr_id)
-            prod2 = ProductMaterialsModel.objects.filter(name_id=pr_id2)
+            try:
+                prod = ProductMaterialsModel.objects.filter(name_id=pr_id)
+                prod2 = ProductMaterialsModel.objects.filter(name_id=pr_id2)
+                prod_name = str(ProductModel.objects.get(id=pr_id))
+                prod_name2 = str(ProductModel.objects.get(id=pr_id2))
+            except:
+                return Response({'status':'wrong data'})
 
             # ombordan bizga k.k mahsulotlarni partiyasidan qatiynazar chaqiryapman
-            # warhou = WareHouseModel.objects.filter(material_id__in=[i.material_id for i in prod])
             warhou = WareHouseModel.objects.all()
 
             needs_list = [count*float(i.quantity) for i in prod]
             needs_id = [i.material_id for i in prod]
             needs_dict = dict(zip(needs_id, needs_list))
             
-            needs_list2 = [count*float(i.quantity) for i in prod2]
+            needs_list2 = [count2*float(i.quantity) for i in prod2]
             needs_id2 = [i.material_id for i in prod2]
             needs_dict2=dict(zip(needs_id2, needs_list2))
 
-            prod_name = str(ProductModel.objects.get(id=pr_id))
-            prod_name2 = str(ProductModel.objects.get(id=pr_id2))
+            
 
 
             for i in warhou:
@@ -63,29 +62,28 @@ def create_product(request):
                         continue
                     if i.material_id==k:
                         z = i.remainder-v
-                        if z<0:
+                        if z < 0:
                             needs_dict[k] = v- i.remainder
                             create_warehouse_serializer(serial_list, i)
 
-                        elif z==0:
+                        elif z == 0:
                             needs_dict[k] = 0
                             create_warehouse_serializer(serial_list, i)
-                            i.remainder = 0
+                            not_free.append(i.id)
 
-
-                        elif z>0:
+                        elif z > 0:
                             i.remainder=i.remainder-v
+
                             create_warehouse_serializer(serial_list, WareHouseModel(id=i.id, material_id=i.material.id, remainder=v, price=i.price))
                             needs_dict[k] = 0
             for k,v in needs_dict.items():
                 if v==0:
                     continue
                 else:
-                    create_warehouse_serializer(serial_list, WareHouseModel(material_id=k, remainder=v,))
-            print(needs_dict2)
+                    create_warehouse_serializer(serial_list, WareHouseModel(material_id=k, remainder=v))
 
             for i in warhou:
-                if i.remainder == 0:
+                if i.id in not_free:
                     continue
                 for k,v in needs_dict2.items():
                     if v==0:
@@ -104,7 +102,6 @@ def create_product(request):
                             i.remainder=i.remainder-v
                             create_warehouse_serializer(serial_list2, WareHouseModel(id=i.id, material_id=i.material.id, remainder=v, price=i.price))
                             needs_dict2[k] = 0
-            print(needs_dict2)
             for k,v in needs_dict2.items():
                 if v==0:
                     continue
@@ -114,8 +111,9 @@ def create_product(request):
             base_data = WarehouseSerializer(serial_list, many=True)
             base_data2 = WarehouseSerializer(serial_list2, many=True)
 
-            # context['result']=[base_data.data,base_data2.data]
             context['result']=[{'product_name':prod_name, 'product_qty':count, 'product_materials':base_data.data},{'product_name':prod_name2, 'product_qty':count2, 'product_materials':base_data2.data}]
 
 
             return Response(context)
+        else:
+            return Response({'status':'wrong data'})
